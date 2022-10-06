e39b15133d7de1172277f065d1b5f6d1bf5ef4b7
andrewb91 <77479121+andrewb91@users.noreply.github.com>
2022-10-05 18:52:43 -0700

    Add files via upload

---
+++ arch/arch1.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+cryptsetup luksFormat /dev/sdb2
+
+cryptsetup open /dev/sdb2 cryptlvm
+
+pvcreate /dev/mapper/cryptlvm 
+
+vgcreate vg1 /dev/mapper/cryptlvm
+
+lvcreate -L 40G vg1 -n root
+lvcreate -L 15G vg1 -n var
+lvcreate -L 16G vg1 -n swap
+lvcreate -L 100G vg1 -n home
+
+mkfs.fat -F32 /dev/sdb1
+mkfs.ext4 /dev/vg1/root
+mkfs.ext4 /dev/vg1/var
+mkfs.ext4 /dev/vg1/home
+mkswap /dev/vg1/swap
+
+mount /dev/vg1/root /mnt
+mkdir /mnt/boot
+mount /dev/sdb1 /mnt/boot
+mkdir /mnt/var
+mount /dev/vg1/var /mnt/var
+mkdir /mnt/home
+mount /dev/vg1/home /mnt/home
+
+swapon /dev/vg1/swap
+
+lsblk
+
+
---
+++ arch/arch2.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+pacstrap /mnt base linux-zen linux-zen-headers linux-firmware vim intel-ucode lvm2
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+arch-chroot /mnt
+
+ln -sf /usr/share/zoneinfo/America/Los_Angeles /etc/localtime
+hwclock --systohc
+sed -i '178s/.//' /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
+#echo "KEYMAP=de_CH-latin1" >> /etc/vconsole.conf
+echo "mobile" >> /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 mobile.localdomain mobile" >> /etc/hosts
+echo root:password | chpasswd
+
+# You can add xorg to the installation packages, I usually add it at the DE or WM install script
+# You can remove the tlp package if you are installing on a desktop or vm
+
+pacman -S --noconfirm grub efibootmgr networkmanager network-manager-applet wireless_tools wpa_supplicant dialog os-prober mtools dosfstools base-devel git bluez bluez-utils pulseaudio-bluetooth cups hplip xdg-utils xdg-users-dirs gufw openssh acpid tlp tlp-rdw
+
+# pacman -S --noconfirm xf86-video-amdgpu
+# pacman -S --noconfirm nvidia nvidia-utils nvidia-settings
+
+grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB #change the directory to /boot/efi is you mounted the EFI partition at /boot/efi
+
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable NetworkManager
+systemctl enable bluetooth
+systemctl enable cups
+systemctl enable fstrim.timer
+systemctl enable tlp.service
+systemctl enable NetworkManager-dispatcher.service
+systemctl enable ssh
+systemctl enable ufw
+systemctl enable acpid
+systemctl mask systemd-rfkill.service systemd-rfkill.socket
+
+useradd -m drew
+echo drew:password | chpasswd
+usermod -aG libvirt ermanno
+
+echo "drew ALL=(ALL) ALL" >> /etc/sudoers.d/ermanno
+
+printf "\e[1;32mDone! Type exit, umount -a and reboot.\e[0m"
+
+
+
